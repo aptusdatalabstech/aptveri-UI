256b989148708082103391bce6ffdcd8643749ed
parvathinathan.v@aptusdatalabs.com <parvathinathan.v@users.noreply.github.com>
2024-01-30 17:20:01 +0530

    New feature

--- src/environments/environment.addev.ts
+++ src/environments/environment.addev.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: false,
     apiKey: 'addevKey',
-    api_prefix: 'http://192.168.1.38:5100/api/',
-    app_url: 'http://192.168.1.38:5100/',
+    api_prefix: 'http://192.168.1.38:8087/api/',
+    app_url: 'http://192.168.1.38:8087/',
     // api_prefix: 'https://aptiveri5dev.aptiveri5.net:8082/api/',
     //   app_url: 'https://aptiveri5dev.aptiveri5.net:8082/',
 };
